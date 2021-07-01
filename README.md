e55c93b6cb3fcdbf4e137c9c0ff6df941055e09e
Andres Asensio Ramos <aasensio@iac.es>
2021-07-01 11:55:45 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# SICON_hinode
-Neural 3D LTE inversor for Hinode data
+# Neural 3D LTE inversor for Hinode
+
+This repository contains an extremely simple way of using [SICON](https://ui.adsabs.harvard.edu/abs/2019A%26A...626A.102A/abstract), the neural inversion method developed by 
+Asensio Ramos & Diaz Baso (2019, A&A, 626, 102A). With only two commands you can
+download Hinode data and get the inversion in just a few minutes.
+
+## Download Hinode data
+
+Search for the initial date and time of the Hinode observation you want. It can
+be done by using the Hinode database ``http://sdc.uio.no/search/form``. Once 
+found, save the date and time of observation and go to the Hinode level 1 database 
+that can be found in `http://www.lmsal.com/solarsoft/hinode/level1hao/`. Enter
+into the subdirectory structure until you find your observation. For instance, 
+let's say you're searching for the observation that started at 19:00 on
+2014/02/04. Go to
+
+    http://www.lmsal.com/solarsoft/hinode/level1hao/2014/02/04/SP3D/20140204_190005
+
+and you will find the data as FITS file. Once located, you can call the 
+`download_hinode.py` command that will download all the FITS files and generate an HDF5 file 
+appropriate for the input to the neural inversor:
+    
+    python download_hinode.py --url http://www.lmsal.com/solarsoft/hinode/level1hao/2014/02/04/SP3D/20140204_190005 --output output.h5 --downloader curl
+
+By default, the files are downloaded with ``wget``. If you do not have it available in your system, 
+which happens in many cases in MacOS, you can use ``curl``. You can find some help of the options 
+of the downloader by using ``python download_hinode -h``.
+
+## Run the code
+
+The code needs as input an HDF5 file with the Stokes profiles downloaded from the Hinode database 
+and will produce as output another HDF5 file with the resulting physical conditions.
+The code is run as:
+
+    python neural_inversion.py --input input.h5 --output output.h5 --normalize 0 100 0 100 --device cpu --resolution 1
+
+The arguments are the input and output files, together with the definition of a box which is used 
+as the quiet Sun for normalizing the Stokes profiles. Additionally, you can decide to do
+the computation on CPU or GPU. Calculations on GPUs will be faster but the field-of-view
+is memory limited. For large maps, we recommend using CPU. Finally, `resolution 1` allows you to output the
+results with the same pixel size as the observations, while `resolution 2` upsamples the solution to
+double resolution. Use it with care.
+
+
+## Requirements
+    bs4
+    numpy
+    argparse
+    h5py
+    tqdm
+    astropy
+    torch
+
+## Use with Anaconda
+
+We recommend to use Anaconda to run this code. We also recommend to generate a new environment in which all the packages will be installed. Then, install ``PyTorch``as indicated in the webpage ``https://pytorch.org/`` depending on your system . A typical process would be:
+
+    conda create -n inversor python=3.8
+    conda activate inversor
+    conda install -c conda-forge numpy h5py tqdm astropy bs4 argparse
+    conda install pytorch torchvision cudatoolkit=10.1 -c pytorch
---
+++ neural_hinode.py
@@ -0,0 +1,247 @@
+import numpy as np
+import matplotlib.pyplot as pl
+import torch
+from torch.nn.modules.module import _addindent
+import h5py
+import argparse
+import glob
+import os
+import sys
+import time
+sys.path.append('models')
+
+import model_x1
+import model_x2
+
+
+class deep_3d_inversion(object):
+    def __init__(self, parsed):
+
+        print('   _____ _____ _____ ____  _   _ ')
+        print('  / ____|_   _/ ____/ __ \| \ | |')
+        print(' | (___   | || |   | |  | |  \| |')
+        print('  \___ \  | || |   | |  | | . ` |')
+        print('  ____) |_| || |___| |__| | |\  |')
+        print(' |_____/|_____\_____\____/|_| \_|')
+                                                                                     
+        self.cuda = torch.cuda.is_available()    
+
+        device = parsed['device']
+        self.superresolution = parsed['resolution']
+
+        if ('cuda' in device):
+            if (self.cuda == False):
+                print('GPU not available. Computing in CPU')
+                device = 'cpu'
+            else:
+                print(f"Computing on GPU {device}")
+
+        if (device == 'cpu'):
+            print("Computing on CPU")
+
+        self.device = torch.device(device)
+               
+        self.ltau = np.array([0.0,-0.5,-1.0,-1.5,-2.0,-2.5,-3.0])
+
+        self.variable = ["T", "v$_z$", "h", "log P", "$(B_x^2-B_y^2)^{1/2}$", "$(B_x B_y)^{1/2}$", "B$_z$"]
+        self.variable_txt = ["T", "vz", "tau", "logP", "sqrtBx2By2", "sqrtBxBy", "Bz"]
+        self.units = ["K", "km s$^{-1}$", "km", "cgs", "kG", "kG", "kG"]
+        self.multiplier = [1.0, 1.e-5, 1.e-5, 1.0, 1.0e-3, 1.0e-3, 1.0e-3]
+
+        self.z_tau1 = 1300.0
+
+
+    def load_weights(self):
+
+        if (self.superresolution == 1):
+            self.checkpoint = 'models/weights_x1.pth'
+            self.normalization = 'models/normalization_x1.npz'
+            print("Generating output x1")
+            print("Defining inversion NN...")
+            self.model = model_x1.block(n_input_channels=112*4, n_output_channels=7*7).to(self.device)
+
+        if (self.superresolution == 2):
+            self.checkpoint = 'models/weights_x2.pth'
+            self.normalization = 'models/normalization_x2.npz'
+            print("Generating output x2")
+            print("Defining inversion NN...")
+            self.model = model_x2.block(n_input_channels=112*4, n_output_channels=7*7).to(self.device)
+
+        
+                                
+        tmp = torch.load(self.checkpoint, map_location=lambda storage, loc: storage)
+                                                
+        self.model.load_state_dict(tmp['inv_state_dict'])        
+        print("=> loaded checkpoint for inversion '{}'".format(self.checkpoint))     
+        self.model.eval()
+                                                                
+        tmp = np.load(self.normalization)
+        self.phys_min, self.phys_max = tmp['minimum'], tmp['maximum']
+
+    def test_hinode(self, parsed):
+
+        print(f"Reading input file {parsed['input']}")
+
+        f = h5py.File(parsed['input'], 'r')
+
+        self.stokes = f['stokes'][:,:,:,:]
+
+        if (parsed['normalize'] is not None):
+            x0, x1, y0, y1 = parsed['normalize']
+            print(f"Data will be normalized to median value in box : {x0}-{x1},{y0}-{y1}")
+            stokes_median = np.median(self.stokes[0,x0:x1,y0:y1,0:3])
+        else:
+            print(f"Data is already normalized")
+            stokes_median = 1.0
+        
+        f.close()
+    
+        print(f"Transposing data")
+        self.stokes = np.transpose(self.stokes, axes=(0,3,1,2))      
+
+        _, n_lambda, nx, ny = self.stokes.shape
+
+        nx_int = nx // 2**4
+        ny_int = ny // 2**4
+        nx = nx_int * 2**4
+        ny = ny_int * 2**4
+        
+        print(f"Size of map {nx} x {ny}")
+        print(f"Cropping map to range (0,{nx})-(0,{ny}) ")
+
+        self.stokes = self.stokes[:,:,0:nx,0:ny]
+
+        print(f"Normalizing data")
+        
+        self.stokes /= stokes_median
+
+        self.stokes[1,:,:,:] /= 0.1
+        self.stokes[2,:,:,:] /= 0.1
+        self.stokes[3,:,:,:] /= 0.1
+
+        self.stokes = np.expand_dims(self.stokes.reshape((4*n_lambda,nx,ny)), axis=0)
+                                                       
+        logtau = np.linspace(0.0, -3.0, 70)
+
+        self.load_weights()
+                
+        print("Running neural network inversion...")
+
+        start = time.time()
+
+        # Do it in two steps in this case
+        if (nx > 512):
+            n = nx // 512
+            idx = []
+            for i in range(n+1):
+                idx.append(i*512)
+            if (nx % 512 != 0):
+                idx.append(nx)
+                extra = 1
+            else:
+                extra = 0
+
+            print(f"Doing in {n} parts : {idx}")
+
+            for i in range(n+extra):
+                input = torch.as_tensor(self.stokes[0:1,:,idx[i]:idx[i+1],:].astype('float32')).to(self.device)
+                print(f"{idx[i]} - {idx[i+1]}")
+                if (i == 0):
+                    with torch.no_grad():                
+                        output_model = self.model(input)
+                        
+                    output_model = output_model.cpu().numpy()                        
+                else:
+                    with torch.no_grad():                
+                        output_model1 = self.model(input)
+
+                    output_model = np.concatenate([output_model, output_model1.cpu().numpy()], axis=-2)                    
+        else:
+
+            input = torch.as_tensor(self.stokes[0:1,:,:,:].astype('float32')).to(self.device)
+                
+            with torch.no_grad():                
+                output_model = self.model(input)
+
+            output_model = output_model.cpu().numpy()
+
+        end = time.time()
+        print(f"Elapsed time : {end-start} s - {1e6*(end-start)/(nx*ny)} us/pixel")
+
+        # Transform the tensors to numpy arrays and undo the transformation needed for the training
+        print("Saving results")
+        output_model = np.squeeze(output_model)
+        output_model = output_model * (self.phys_max[:,None,None] - self.phys_min[:,None,None]) + self.phys_min[:,None,None]    
+        output_model = output_model.reshape((7,7,self.superresolution*nx,self.superresolution*ny))
+
+        
+        tmp = '.'.join(self.checkpoint.split('/')[-1].split('.')[0:2])
+        f = h5py.File(f"{parsed['output']}", 'w')
+        db_logtau = f.create_dataset('tau_axis', self.ltau.shape)
+        db_T = f.create_dataset('T', output_model[0,:,:,:].shape)
+        db_vz = f.create_dataset('vz', output_model[1,:,:,:].shape)
+        db_tau = f.create_dataset('tau', output_model[2,:,:,:].shape)
+        db_logP = f.create_dataset('logP', output_model[3,:,:,:].shape)
+        db_Bx2_By2 = f.create_dataset('sqrt_Bx2_By2', output_model[4,:,:,:].shape)
+        db_BxBy = f.create_dataset('sqrt_BxBy', output_model[5,:,:,:].shape)
+        db_Bz = f.create_dataset('Bz', output_model[6,:,:,:].shape)
+        db_Bx = f.create_dataset('Bx', output_model[4,:,:,:].shape)
+        db_By = f.create_dataset('By', output_model[5,:,:,:].shape)
+
+        Bx = np.zeros_like(db_Bz[:])
+        By = np.zeros_like(db_Bz[:])
+                    
+
+        db_logtau[:] = self.ltau
+        db_T[:] = output_model[0,:,:,:] * self.multiplier[0]
+        db_vz[:] = output_model[1,:,:,:] * self.multiplier[1]
+        db_tau[:] = output_model[2,:,:,:] * self.multiplier[2]
+        db_logP[:] = output_model[3,:,:,:] * self.multiplier[3]
+        db_Bx2_By2[:] = output_model[4,:,:,:] * self.multiplier[4]
+        db_BxBy[:] = output_model[5,:,:,:] * self.multiplier[5]
+        db_Bz[:] = output_model[6,:,:,:] * self.multiplier[6]
+
+        A = np.sign(db_Bx2_By2[:]) * db_Bx2_By2[:]**2    # I saved sign(Bx^2-By^2) * np.sqrt(Bx^2-By^2)
+        B = np.sign(db_BxBy[:]) * db_BxBy[:]**2    # I saved sign(Bx*By) * np.sqrt(Bx*By)
+
+    # This quantity is obviously always >=0
+        D = np.sqrt(A**2 + 4.0*B**2)
+        
+        ind_pos = np.where(B >0)
+        ind_neg = np.where(B < 0)
+        ind_zero = np.where(B == 0)
+        Bx[ind_pos] = np.sign(db_BxBy[:][ind_pos]) * np.sqrt(A[ind_pos] + D[ind_pos]) / np.sqrt(2.0)
+        By[ind_pos] = np.sqrt(2.0) * B[ind_pos] / np.sqrt(1e-1 + A[ind_pos] + D[ind_pos])
+        Bx[ind_neg] = np.sign(db_BxBy[:][ind_neg]) * np.sqrt(A[ind_neg] + D[ind_neg]) / np.sqrt(2.0)
+        By[ind_neg] = -np.sqrt(2.0) * B[ind_neg] / np.sqrt(1e-1 + A[ind_neg] + D[ind_neg])
+        Bx[ind_zero] = 0.0
+        By[ind_zero] = 0.0
+
+        db_Bx[:] = Bx
+        db_By[:] = By
+
+        f.close()
+        
+if (__name__ == '__main__'):
+
+    parser = argparse.ArgumentParser(description='Fast 3D LTE inversion of Hinode datasets')
+    parser.add_argument('-i', '--input', default=None, type=str,
+                    metavar='INPUT', help='Input file', required=True)
+    parser.add_argument('-o', '--output', default=None, type=str,
+                    metavar='OUTPUT', help='Output file', required=True)
+    parser.add_argument('-n', '--normalize', default=None, type=int, nargs='+',
+                    metavar='OUTPUT', help='Output file', required=False)
+    parser.add_argument('-d', '--device', default='cpu', type=str, 
+                    metavar='DEVICE', help='Device : cpu/cuda:0/cuda:1,...', required=False)
+    parser.add_argument('-r', '--resolution', default=1, type=int, choices=[1,2],
+                    metavar='RESOLUTION', help='Resolution', required=False)
+
+    parsed = vars(parser.parse_args())
+
+    if (not os.path.exists(parsed['output'])):
+
+        deep_network = deep_3d_inversion(parsed)
+
+        deep_network.test_hinode(parsed)
+    else:
+        print(f"Output file {parsed['output']} already exists. Remove it to recompute.")
